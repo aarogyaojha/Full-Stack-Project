8e37b7c92f4f64d02cfb09238a4eed64cdf9335b
aarogya-ojha <aarogya@abblor.com>
2025-01-08 18:41:23 +0530

    added the routes for accessing apis

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
-	"fmt"
+	routes "Full-Stack-Project/routes"
+	"os"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Starting server on :8080")
+	routes.SetupRoutes()
+	port:=os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+
+	router :=gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1",func(ctx *gin.Context) {
+		ctx.JSON(200,gin.H{"sucess":"Access granted for api 1"})
+	})
+
+	router.GET("/api-2", func(ctx * gin.Context){
+		ctx.JSON(200,gin.H{"success":"Access granted for api 2"})
+	})
+
+	router.Run(":"+ port)
 }
