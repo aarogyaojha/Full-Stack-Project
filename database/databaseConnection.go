67727700a197db57090dfe3b6e66526bcf9ffc02
aarogyaojha <arogyaojha251@gmail.com>
2025-01-10 16:35:07 +0530

    database completion part

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,49 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"time"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error getting env, %v", err)
+	}
+
+	Mongodb := os.Getenv("MONGODB_URL")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(Mongodb))
+
+	if err != nil {
+		log.Fatalf("Error getting mongo client: %v", err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatalf("Error connecting to mongo: %v", err)
+	}
+
+	fmt.Print("connected to MONGODB")
+
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("cluster0").Collection(collectionName)
+	return collection
+}
